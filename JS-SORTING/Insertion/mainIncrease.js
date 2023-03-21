9bae7f046a826f69dc9f18cc9ab06194e00c7369
Silv <forenks1005@gmail.com>
2023-03-21 08:00:04 -0500

    Appeding new file

---
+++ JS-SORTING/Insertion/mainIncrease.js
@@ -0,0 +1,27 @@
+let counter  = 0;
+let array = [];
+
+console.time('Insertion');
+
+for(let i = 0; i < 100; i++){
+    array[i] = Math.floor(Math.random()*10000);
+  
+    }
+
+for(let i = 1; i < array.length; i++){
+
+    let j = i - 1;
+    let temp = array[i];
+
+    while(j >= 0 && array[j] > temp){
+        array[j + 1] = array[j];
+        j--;
+        counter++
+    }
+    array[j + 1] = temp;
+}
+
+console.timeEnd('Insertion');
+console.log("Number of iterations: " + counter);
+console.log(array);
+
---
+++ JS-SORTING/random.js
@@ -0,0 +1,14 @@
+let counter  = 0;
+let array = [];
+
+console.time('random');
+
+for(let i = 0; i < 100; i++){
+    array[i] = Math.floor(Math.random()*10000);
+    counter++;
+    }
+
+console.timeEnd('random')
+console.log("Number of iterations: " + counter)
+console.log(array)
+
